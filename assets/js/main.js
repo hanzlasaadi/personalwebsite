478b3deef64cb7f47fdcf858fa7d1125f7fdcc26
hanzlasaadi <hanzalasaadi@gmail.com>
2023-01-30 22:13:40 +0500

    Skills completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,23 @@ function linkAction() {
 navLinks.forEach((link) => link.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills__content"); //gives a html collection
+const skillsHeader = document.querySelectorAll(".skills__header"); //gives a nodelist
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass == "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((val) => {
+  val.addEventListener("click", toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
