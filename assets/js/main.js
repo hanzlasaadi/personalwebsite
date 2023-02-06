0405efca9d7e9b179708a43f084064fc30079b3a
hanzlasaadi <hanzalasaadi@gmail.com>
2023-02-07 00:13:18 +0500

    projects, testimonials and contact me

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ modalCloses.forEach((modalClose, i) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
   navigation: {
@@ -109,6 +109,23 @@ let swiper = new Swiper(".portfolio__container", {
 });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
